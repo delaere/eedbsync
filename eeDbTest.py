bb3193c258587578dd6e5a69fe1056e601ccfe50
Christophe Delaere <christophe.delaere@cern.ch>
2016-01-06 18:12:35 +0100

    Simple test for the eeDb API
    Exactly the same as for the eedomusAPI, just a different API loaded at
    the beginning.

---
+++ eeDbTest.py
@@ -0,0 +1,33 @@
+#!/bin/env python
+
+from eedomus import findDevice
+from eedb import eeDbAPI
+
+# the API entry point
+api = eeDbAPI()
+
+if api.authTest()==1:
+	print "Authentification OK"
+else:
+	print "Authentification Error"
+
+# get the list of devices
+devs = api.getPeriphList()
+
+# pick one for the next operations
+dev = findDevice(devs,room_name="Salon")[4]
+
+# print infos on that device
+print dev
+
+# get value and last change
+print "Value:", dev.lastValue(),
+print ", last changed on", dev.lastValueChange()
+
+# get the history and print some information
+history = dev.getHistory()
+print "History size: ", len(history)
+print "First entry: ",history[0]
+print "Last entry:", history[-1]
+
+
