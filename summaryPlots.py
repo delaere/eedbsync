a44728bf19f63507e92209ae1ecf56ab7fc6e8c9
Christophe Delaere <christophe.delaere@cern.ch>
2016-02-29 22:46:39 +0100

    Reworked the rebin methods
    This is now simpler and more robust.
    There is a unique method that bins data when needed. As soon as data
    comes binned (by default 15minutes), it can be trivially grouped in
    smaller units (hours, days, months, years).

--- summaryPlots.py
+++ summaryPlots.py
@@ -15,90 +15,7 @@ def pairwise(iterable):
     next(b, None)
     return izip(a, b)
 
-def cummulative(inputgraph, conversionFactor = 3600000., samplingPeriod = None):
-	""" Integrates the input to return a cummulative distribution.
-	    By default, it uses the trapeze integration rule.
-	    If samplingPeriod is set, each point is taken independently over that time.
-	    The default conversion factor works for both electricity (watt*s -> kWh) and gaz (dm3/h*s -> m3)
-	"""
-	output = ROOT.TGraph()
-	timevector = inputgraph.GetX()
-	valuesvector = inputgraph.GetY()
-	data = []
-	last = None
-	for i in range(inputgraph.GetN()-1,0,-1):
-		data.append((timevector[i],valuesvector[i]))
-	for i,((t0,d0),(t1,d1)) in enumerate(pairwise(data)):
-		if last is None:
-			last = 0
-			output.SetPoint(i,t0,0)
-		if samplingPeriod is None:
-			last += (((d0+d1)/2.)*(t1-t0))/conversionFactor
-		else:
-			last += d1*samplingPeriod/conversionFactor
-		output.SetPoint(i+1,t1,last)
-	return output
-
-def graphFromHistory(history):
-    time = ROOT.TDatime()
-    graph = ROOT.TGraph()
-    for i,(to,do) in enumerate(history):
-        time.Set(do.strftime("%Y-%m-%d %H:%M:%S"))
-        graph.SetPoint(i,time.Convert(),ROOT.Double(float(to)))
-    return graph
-
-# method to rebin data.
-# it doesn't work for cumulative data.
-#TODO rework the rebin method: need to fix the start, and consider different modes as for the cummulative method
-#TODO: add mode for min/max
-def rebin(data, period):
-  # TODO: order data first?
-
-  output = [ (0,data[-1][1]),  ]
-
-  for ((d0,time0),(d1,time1)) in pairwise(data[::-1]):
-    lasttime = output[-1][1]
-    nexttime = lasttime + period
-    d0 = float(d0)
-    d1 = float(d1)
-
-    # if the previous data point is before this interval, add a correction to the previous interval
-    # this should never happen...
-    if time0 < lasttime:
-      print "WARNING: I should never come here..."
-      duration = (lasttime-time0).total_seconds()
-      output[-2] = (output[-2][0] + d1*duration/period.total_seconds(), output[-2][1])
-    else:
-      lasttime = time0
-
-    # fill the "full intervals
-    while time1 > nexttime:
-      duration = (nexttime - lasttime).total_seconds()
-      output[-1] = (output[-1][0] +d1*duration/period.total_seconds(),output[-1][1])
-      output.append((0,nexttime)) 
-      lasttime = output[-1][1]
-      nexttime = lasttime + period
-
-    # the last bit
-    duration = (time1-lasttime).total_seconds() #TODO there is probably a bug here: lasttime is not what we have to use if time0 is closer
-    output[-1] = (output[-1][0] +d1*duration/period.total_seconds(), output[-1][1])
-  
-  return output[:-1]
-
-def perHour(data):
-	return rebin(data,timedelta(0,3600))
-
-def perDay(data):
-	return rebin(data,timedelta(1))
-
-def perWeek(data):
-	return rebin(data,timedelta(7))
-
-def perMonth(data):
-	return rebin(data,timedelta(30))
-
-#TODO: add a cleaning method for the history: drop zero values, etc.
-
+# computes the degree-jours per day.
 def degreeJours(t_ext, t_ref = 15, t_max = 15):
 
   dataStart = t_ext[-1][1].replace(hour=0, minute=0, second=0, microsecond=0) 
@@ -129,26 +46,64 @@ def degreeJours(t_ext, t_ref = 15, t_max = 15):
   
   return output[:-1]
 
-def monthlySum(data):
+
+#TODO: add a cleaning method for the history: drop zero values, etc.
+
+# method to bin unbinned data.
+# it should not be used on already binned or cumulative data.
+def binHistory(data, period = timedelta(minutes=15)):
+
+  sorted_data = sorted(data,key=operator.itemgetter(1), reverse=True)
+  output = [ (0,sorted_data[-1][1]), ]
+
+  for ((d0,time0),(d1,time1)) in pairwise(sorted_data[::-1]):
+    lasttime = output[-1][1]
+    nexttime = lasttime + period
+    d0 = float(d0)
+
+    # this should never happen with sorted data
+    assert(time0>=lasttime)
+
+    # fill the full intervals
+    while time1 > nexttime:
+      duration = (nexttime - lasttime).total_seconds() if time0<lasttime else (nexttime-time0).total_seconds()
+      output[-1] = (output[-1][0] +d0*duration/period.total_seconds(),output[-1][1])
+      output.append((0,nexttime)) 
+      lasttime = output[-1][1]
+      nexttime = lasttime + period
+
+    # the last bit
+    duration = (time1-lasttime).total_seconds() if time0<lasttime else (time1-time0).total_seconds()
+    output[-1] = (output[-1][0] +d0*duration/period.total_seconds(), output[-1][1])
+  
+  return output[:-1]
+
+from enum import Enum
+class timeGranularity(Enum):
+	year = 1
+	month = 2
+	day = 3
+	hour = 4
+
+# this is the best way to rebin data that are already integrated on a fixed period, like gaz consumption or degrees-jours
+# but it doesn't work when the data points are instant measurements like electricity
+# for these, first use the binHistory method above
+#TODO: add mode for min/max
+def rebin(data, granularity):
+	assert isinstance(granularity,timeGranularity)
 	output = {}
 	for (entry, timestamp) in data:
-		month = date(timestamp.year, timestamp.month, 1)
-		if not month in output:
-			output[month] = float(entry)
+		if granularity==timeGranularity.hour:
+			time = datetime(timestamp.year, timestamp.month, timestamp.day, timestamp.hour, 0, 0)
 		else:
-			output[month] += float(entry)
+			time = date(timestamp.year, timestamp.month if granularity!=timeGranularity.year else 0, timestamp.day if granularity==timeGranularity.day else 0)
+		if not time in output:
+			output[time] = float(entry)
+		else:
+			output[time] += float(entry)
 	return output
 
-def dailySum(data):
-	output = {}
-	for (entry, timestamp) in data:
-		day = date(timestamp.year, timestamp.month, timestamp.day)
-		if not day in output:
-			output[day] = float(entry)
-		else:
-			output[day] += float(entry)
-	return output
-
+# make an histogram from the methods above
 def histogramFromDictionnary(data, name="histo",title="histo", binLabel="%B %Y"):
 	h = ROOT.TH1F(name,title,len(data),0,len(data))
 	sorted_data = sorted(data.items(), key=operator.itemgetter(0))
@@ -156,6 +111,52 @@ def histogramFromDictionnary(data, name="histo",title="histo", binLabel="%B %Y")
 		h.SetBinContent(i+1,value)
 		h.GetXaxis().SetBinLabel(i+1,date.strftime(binLabel))
 	return h
+
+# make a graph from the methods above
+def graphFromDictionnary(data):
+    time = ROOT.TDatime()
+    graph = ROOT.TGraph()
+    sorted_data = sorted(data.items(), key=operator.itemgetter(0))
+    for i,(itime,value) in enumerate(sorted_data):
+        time.Set(itime.strftime("%Y-%m-%d %H:%M:%S"))
+        graph.SetPoint(i,time.Convert(),ROOT.Double(value))
+    return graph
+
+def graphFromHistory(history):
+    time = ROOT.TDatime()
+    graph = ROOT.TGraph()
+    for i,(to,do) in enumerate(history):
+        time.Set(do.strftime("%Y-%m-%d %H:%M:%S"))
+        graph.SetPoint(i,time.Convert(),ROOT.Double(float(to)))
+    return graph
+
+# makes a cumulative plot from a time graph
+def cummulative(inputgraph, conversionFactor = 3600000., samplingPeriod = None):
+	""" Integrates the input to return a cummulative distribution.
+	    By default, it uses the trapeze integration rule.
+	    If samplingPeriod is set, each point is taken independently over that time.
+	    The default conversion factor works for both electricity (watt*s -> kWh) and gaz (dm3/h*s -> m3)
+	"""
+	output = ROOT.TGraph()
+	timevector = inputgraph.GetX()
+	valuesvector = inputgraph.GetY()
+	data = []
+	last = None
+	for i in range(inputgraph.GetN()-1,0,-1):
+		data.append((timevector[i],valuesvector[i]))
+	for i,((t0,d0),(t1,d1)) in enumerate(pairwise(data)):
+		if last is None:
+			last = 0
+			output.SetPoint(i,t0,0)
+		if samplingPeriod is None:
+			last += (((d0+d1)/2.)*(t1-t0))/conversionFactor
+		else:
+			last += d0*samplingPeriod/conversionFactor
+		output.SetPoint(i+1,t1,last)
+	return output
+
+################################################################################
+################################################################################
 
 # login
 api = eeDbAPI()
@@ -204,8 +205,10 @@ c3.cd(3)
 gr0 = cummulative(gr0)
 igraphs.append(gr0)
 gr0.Draw()
+#ajouter histogramme de conso par semaine et par mois
 
 # conso gaz
+# http://www.energieplus-lesite.be/index.php?id=10016
 gaz = findDevice(devs,usage_name=u"Compteur de gaz")[0]
 gazcanvas =  ROOT.TCanvas()
 gazcanvas.Divide(2,2)
@@ -214,7 +217,7 @@ gazgr = graphFromHistory(gaz.getHistory())
 gazgrc = cummulative(gazgr, 3600000, 900)
 gazgrc.Draw()
 gazcanvas.cd(1)
-gazhdata = dailySum(gaz.getHistory())
+gazhdata = rebin(gaz.getHistory(),timeGranularity.day)
 gazh = histogramFromDictionnary(gazhdata, name="gaz",title="Consommation gaz",binLabel="%d %B %Y")
 gazh.Scale(1/4000.)
 gazh.Draw()
@@ -233,4 +236,4 @@ for degree,timestamp in dj:
 performance.SetMarkerStyle(ROOT.kFullCircle)
 performance.Draw("AP")
 performance.Fit("pol1")
-
+#TODO repeter par semaine et par mois
