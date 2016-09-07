e4ed1db043404454e52086aa5f94f74a04d30b06
Christophe Delaere <christophe.delaere@cern.ch>
2016-09-07 13:48:12 +0200

    Options handling for eedb2eetsdb migration script
    (incl. dry option)

--- eedb2eetsdb.py
+++ eedb2eetsdb.py
@@ -7,10 +7,12 @@ from opentsdbclient.opentsdberrors import OpenTSDBError
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
+from optparse import OptionParser
+
 import pprint
 import time
 
-def migrate(replace=False,yearsback=10):
+def migrate(replace=False,yearsback=10,dryrun=False):
     api = eeDbAPI()
     devices = api.getPeriphList()
     eetsdb  = eeTSDB('localhost',4242)
@@ -22,7 +24,7 @@ def migrate(replace=False,yearsback=10):
             continue
         # check if the TS is there already. If yes, look for the last point and continue from there
         ts = eetsdb.mkTimeseries(dev)
-        eetsdb.registerTS(ts)
+        if not dryrun: eetsdb.registerTS(ts)
         begin = datetime.now()-relativedelta(years=yearsback)
         try:
             res = client.search("LOOKUP",metric=ts.metric, tags=ts.tags)
@@ -37,7 +39,7 @@ def migrate(replace=False,yearsback=10):
                 sq = OpenTSDBtsuidSubQuery("sum",[tsuid])
                 if replace:
                     query = OpenTSDBQuery([sq],"%dy-ago"%yearsback,delete=True)
-                    answer = client.query(query)
+                    if not dryrun: answer = client.query(query)
                     begin = datetime.now()-relativedelta(years=yearsback)
                     time.sleep(5)
                 else:
@@ -48,13 +50,35 @@ def migrate(replace=False,yearsback=10):
                         begin = datetime.fromtimestamp(last+1)
             # migrate that dev
             print "migrating",dev.periph_id, dev.name, dev.room_name, dev.usage_name, "from", begin
-            eetsdb.migrate(device=dev,start_date=begin, end_date=None)
+            if not dryrun: eetsdb.migrate(device=dev,start_date=begin, end_date=None)
         except OpenTSDBError as e:
             print "Exception while processing",dev.periph_id, dev.name, dev.room_name, dev.usage_name,"Skipping."
             raise
 
-# TODO: add command-line options
-# replace=False, yearsback=10, dryRun=False
+class MyOptionParser: 
+    def __init__(self):
+        usage  = "Usage: %prog [options]"
+        self.parser = OptionParser(usage=usage)
+        self.parser.add_option("-r","--replace", action="store_true", 
+                               dest="replace", default=False,
+             help="replace existing values")
+        self.parser.add_option("-y","--yearsback", action="store", type="int",
+                               dest="yearsback", default=10,
+             help="years back in history")
+        self.parser.add_option("-d","--dry", action="store_true",
+                               dest="dryrun", default=False,
+             help="dry run (do not touch the database)")
+
+    def get_opt(self):
+        """
+        Returns parse list of options
+        """
+        opts, args = self.parser.parse_args()
+        return opts
+
 if __name__ == "__main__":
-    migrate()
+    # get the options
+    optmgr = MyOptionParser()
+    opts = optmgr.get_opt()
+    migrate(**opts.__dict__)
 
